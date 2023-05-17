a7298b37517af9f83d6dd6f5820e97ea08d2669a
Tawonga netcode <netcodemw@gmail.com>
2023-05-17 15:46:23 +0200

    add: community urls

--- Community/urls.py
+++ Community/urls.py
@@ -5,19 +5,14 @@ urlpatterns=[
     path('', views.CommunityList.as_view()),
     path('Community/<int:pk>/', views.CommunityDetail.as_view()),
     path('Community/', views.CommunityList.as_view()),
-
     path('Citizen/<int:pk>/', views.CitizenDetail.as_view()),
     path('Citizen/', views.CitizenList.as_view()),
-
     path('CommunityLeader/<int:pk>/', views.CommunityLeaderDetail.as_view()),
     path('CommunityLeader/', views.CommunityLeaderList.as_view()),
-
     path('Household/<int:pk>/', views.HouseholdDetail.as_view()),
     path('Household/', views.HouseholdList.as_view()),
-
     path('HouseMember/<int:pk>/', views.HousememberDetail.as_view()),
     path('HouseMember/', views.HousememberList.as_view()),
-
     path('Member/<int:pk>/', views.MemberDetail.as_view()),
     path('Member/', views.MemberList.as_view()),
 ]
